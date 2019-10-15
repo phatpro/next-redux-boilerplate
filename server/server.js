dbf9e82bf976f054c96d2a8ba183ea8197402106
hoa phat <hoaphat.nguyenphan@gmail.com>
2019-10-16 06:43:16 +0700

    initial commit
    hihi

---
+++ server/server.js
@@ -0,0 +1,46 @@
+/*eslint-env node*/
+var lines = process.stdout.getWindowSize()[1];
+for (var i = 0; i < lines; i++) {
+	console.log('\r\n');
+}
+
+require('colors');
+const argv = require('minimist')(process.argv.slice(2));
+console.log('Nạp argv', argv);
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const sitemap = require('sitemap');
+
+const next = require('next');
+const nextApp = next({
+	dev: argv.env === 'development'
+});
+const nextAppHandler = nextApp.getRequestHandler();
+
+nextApp
+	.prepare()
+	.then(() => {
+		app.get('/', (req, res) => {
+			return nextApp.render(req, res, '/home');
+		});
+
+		app.get('*', (req, res) => {
+			return nextAppHandler(req, res);
+		});
+
+		app.use((error, req, res, next) => {
+			console.log('App broken'.red, error);
+		});
+
+		app.listen(argv.port, argv.hostname, () => {
+			console.log(`App running on port: ${argv.port}, hostname: ${argv.hostname}`.green);
+		});
+	})
+	.catch((error) => {
+		console.log('App startup error'.red);
+		throw error;
+	});
